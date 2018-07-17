7283d1e86a512b1b830df37488952aa179a3bb81
Ian Yamey <ianyamey@gmail.com>
2018-07-17 14:10:43 -0400

    Display the minimum and maximum order value in the widget

--- app/helpers/spree/quad_pay_helper.rb
+++ app/helpers/spree/quad_pay_helper.rb
@@ -1,50 +1,88 @@
 module Spree
   module QuadPayHelper
-    def quad_pay_widget(type, amount)
-      min_amount = Spree::Config.quad_pay_min_amount.to_f
-      max_amount = Spree::Config.quad_pay_max_amount.to_f
-      order_total = current_order.total.to_f rescue 0
+    QUADPAY_WIDGET_URL_BASE = "https://widgets.quadpay.com"
 
-      # Logic show/hide widget
-      # Cart page the logic should be: "IF (cart.total_price > min_order_total)
-      #   THEN show_widget"
-      # Product page the logic should be: "IF (product.price > min_order_total)
-      #   OR (cart.total_price > min_order_total) THEN show_widget"
-      condition_on_cart = min_amount < order_total && order_total < max_amount
-      condition_on_amount = min_amount < amount && amount < max_amount
-      return if %w(cart checkout).include?(type) && !condition_on_cart
-      return if type == 'product' && !condition_on_amount && !condition_on_cart
-
-      if qpm = Spree::BillingIntegration::QuadPayCheckout.available_on_front_end.active.first
-        display_widget =
-          case type
-            when 'product'
-              Spree::Config.quad_pay_display_widget_at_product_page
-            when 'cart'
-              Spree::Config.quad_pay_display_widget_at_cart_page
-            when 'checkout'
-              Spree::Config.quad_pay_display_widget_at_checkout_process
-            end
-        if display_widget
-          payment_amount = (amount / 4.0 * 100).to_i / 100.0
-          url = "https://widgets.quadpay.com/#{Spree::Config.quad_pay_merchant_name}/quadpay-widget-0.1.0.js?type=calculator&min=#{min_amount}&max=#{max_amount}&amount=#{payment_amount}"
-          widget_html(number_to_currency(payment_amount), url).html_safe
-        end
-      end
+    def quadpay_active_on_front_end?
+      Spree::BillingIntegration::QuadPayCheckout.available_on_front_end.exists?(active: true)
     end
 
-    def widget_html(amount, widget_url)
-      <<-HTML
+    def display_quadpay_widget_on_product_page?
+      return false unless quadpay_active_on_front_end?
+
+      Spree::Config.quad_pay_display_widget_at_product_page
+    end
+
+    def display_quadpay_widget_on_cart_page?
+      return false unless current_order
+      return false unless quadpay_active_on_front_end?
+
+      Spree::Config.quad_pay_display_widget_at_cart_page
+    end
+
+    def display_quadpay_widget_on_checkout_page?
+      return false unless current_order
+      return false unless quadpay_active_on_front_end?
+
+      Spree::Config.quad_pay_display_widget_at_checkout_process
+    end
+
+    def quadpay_product_widget(product_price)
+      return '' unless product_price
+
+      order_total = current_order ? current_order.total.to_f : 0
+
+      quadpay_widget(
+        amount: product_price,
+        future_order_total: order_total + product_price
+      )
+    end
+
+    def quadpay_cart_widget
+      return '' unless current_order
+
+      quadpay_widget(
+        amount: current_order.total,
+        future_order_total: current_order.total
+      )
+    end
+
+    def quadpay_widget(amount:, future_order_total:)
+      min_amount = Spree::Config.quad_pay_min_amount.to_f
+      max_amount = Spree::Config.quad_pay_max_amount.to_f
+
+      installment_amount = (amount / 4.0 * 100).to_i / 100.0
+      widget_url = quadpay_widget_url(min: min_amount, max: max_amount, amount: amount)
+
+      body = if future_order_total > max_amount
+          "on orders below #{number_to_currency(max_amount)}"
+        elsif future_order_total < min_amount
+          "on orders above #{number_to_currency(min_amount)}"
+        else
+          "of <span style='font-weight: bold;'>#{number_to_currency(installment_amount)}</span>"
+        end
+
+      widget_html = <<-HTML
         <!-- QuadPay Product Page Widget START -->
-        <div id='quadPayCalculatorWidget' class='yui3-cssreset' style='min-height: 40px; padding: 7px 0; width: 100%; max-width: 350px; color: black; text-transform: none; box-sizing: inherit;'>
-          <img id='quadPayCalculatorWidgetLogo' src='https://assets.quadpay.com/assets/images/quadpay-logo-actually-black-tight-200.png' style='width: 90px; float: right;'>
-          <div id='quadPayCalculatorWidgetText' style='position: relative; text-align: left;'>or 4 Interest-Free Payments
-            <span id='quadPayCalculatorWidgetTextFromCopy' sytle='margin-right: 10px;'>of <span style='font-weight: bold;'>#{amount}</span></span>
+        <div id='quadPayCalculatorWidget'>
+          <p id='quadPayCalculatorWidgetText'>
+            or 4 Interest-Free Payments
+            #{body}
+            with
+            <img id='quadPayCalculatorWidgetLogo' src='https://assets.quadpay.com/assets/images/quadpay-logo-actually-black-tight-200.png' style='width: 90px;'>
+            <div id=''
           </div>
-          <script async src='#{widget_url}' type='application/javascript'></script>
+          <div id="quadPayCalculatorWidgetLearn" style="font-size: smaller; text-align: right; cursor: pointer; margin-top: -4px; margin-right: 2px;">
+            <script async src='#{widget_url}' type='application/javascript'></script>
+          </div>
         </div>
         <!-- QuadPay Product Page Widget END -->
       HTML
+
+      widget_html.html_safe
+    end
+
+    def quadpay_widget_url(min:, max:, amount:)
+      "#{QUADPAY_WIDGET_URL_BASE}#{Spree::Config.quad_pay_merchant_name}/quadpay-widget-0.2.0.js?type=calculator&min=#{min}&max=#{max}&amount=#{amount}"
     end
   end
 end
