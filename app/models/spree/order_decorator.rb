ea4ed0d7fe3425904c4555cb93d875eeb8fcdaf1
kolbaa <kolba.mail.by284@gmail.com>
2019-01-30 13:24:29 +0300

    Fix undefined method for array

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -5,9 +5,9 @@ Spree::Order.class_eval do
     qpm_ids = Spree::BillingIntegration::QuadPayCheckout.where(active: true).ids
     @available_payment_methods ||=
       if qpm_ids.any? && (self.total < Spree::Config.quad_pay_min_amount.to_f || self.total > Spree::Config.quad_pay_max_amount.to_f)
-        Spree::PaymentMethod.available.where.not(id: qpm_ids)
+        Spree::PaymentMethod.where(active: true).where.not(id: qpm_ids)
       else
-        Spree::PaymentMethod.available
+        Spree::PaymentMethod.where(active: true)
       end
   end
 end
