e32a102c9f0bbfbbfa276a2ca8291ed6aeaad45c
Maksim Shilov <storm25132513@gmail.com>
2020-02-25 17:59:33 +0300

    remove unnecessary decorators

--- app/controllers/spree/admin/refunds_controller_decorator.rb
+++
@@ -1,9 +0,0 @@
-Spree::Admin::RefundsController.class_eval do
-  after_action :check_error_flashes, only: :create
-
-  private
-    def check_error_flashes
-      error_msg = @object.errors.full_messages.join('. ')
-      flash[:error] = error_msg if error_msg.present?
-    end
-end
